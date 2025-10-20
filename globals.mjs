36839fa40575616761b04609b032e62c23b05c08
conferenceal <appconferenceal@gmail.com>
2025-10-20 11:34:14 +0700

    Globals

---
+++ globals.mjs
@@ -0,0 +1,7 @@
+// GLOBALS
+// Di dalam NodeJS, terdapat library berupa variable atau function yang secara global bisa di akses dimana saja tanpa harus melakukan import
+// https://nodejs.org/dist/latest-v16.x/docs/api/globals.html
+
+setTimeout(function () {
+  console.log("Hello World");
+}, 2000);
